824457b96dad37841c7b4832113a572e706a34ee
tunnckoCore <mameto_100@mail.bg>
2017-02-19 21:57:34 +0200

    chore(init): initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * mich <https://github.com/tunnckoCore/mich>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (https://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function mich () {
+
+}
---
+++ test.js
@@ -0,0 +1,18 @@
+/*!
+ * mich <https://github.com/tunnckoCore/mich>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (https://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+const test = require('mukla')
+const mich = require('./index')
+
+test('mich', (done) => {
+  mich()
+  done()
+})
